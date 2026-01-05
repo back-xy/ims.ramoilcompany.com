ae93d2d1b13e5a554793b7da04681ef84620bdf2
Bawar <bawarmohamad100@gmail.com>
2026-01-06 02:32:25 +0300

    updating user model to no require verified email

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@ramoilcompany.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@ramoilcompany.com');
     }
 
     /**
